ce2a70ed97d8d56a73e248f2405c77df28595c6a
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-29 16:54:39 +0200

    over70gradeslist

--- snack2/snack2.js
+++ snack2/snack2.js
@@ -19,37 +19,37 @@ const students = [
     {
         id: 213,
         name: 'Marco della Rovere',
-        grades: '78',
+        grades: 78,
     },
     {
         id: 110,
         name: 'Paola Cortellessa',
-        grades:'96',
+        grades:96,
     },
     {
         id: 250,
         name: 'Andrea Mantegna',
-        grades: '48',
+        grades: 48,
     },
     {
         id: 145,
         name: 'Gaia Borromini',
-        grades: '74',
+        grades: 74,
     },
     {
         id: 196,
         name: 'Luigi Grimaldello',
-        grades: '68',
+        grades: 68,
     },
     {
         id: 102,
         name: 'Piero della Francesca',
-        grades: '50',
+        grades: 50,
     },
     {
         id: 120,
         name: 'Francesca da Polenta',
-        grades: '84',
+        grades: 84,
     },
 ];
 console.dir(students)
@@ -58,3 +58,7 @@ console.dir(students)
 const uppercaseNames = students.map(student => student.name.toUpperCase());
 
 console.log(uppercaseNames);
+
+//preparo la lista di chi ha un voto superiore a 70 col metodo filter
+const gradesOver70 = students.filter(student => student.grades > 70);
+console.dir(gradesOver70);
