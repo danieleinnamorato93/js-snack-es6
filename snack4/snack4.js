95cbced1d27318e16dd0d5e865f21b20bd1f270b
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-30 16:25:50 +0200

    array done

---
+++ snack4/snack4.js
@@ -0,0 +1,28 @@
+const teamsFootball = [ 
+    {
+        name:'Lipsia',
+        points:82,
+        fouls:9,
+
+    },
+    {
+        name:'Liverpool',
+        points:90,
+        fouls:10,
+
+    },
+    {
+        name:'Getafe',
+        points:30,
+        fouls:3,
+
+    },
+    {
+        name:'Palermo',
+        points:60,
+        fouls:25,
+
+    },
+    
+]
+console.table(teamsFootball)
