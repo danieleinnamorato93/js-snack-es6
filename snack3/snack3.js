1e7955e32483f48d1e1f91a384401c3e52086fbb
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-30 15:25:41 +0200

    array created

---
+++ snack3/snack3.js
@@ -0,0 +1,20 @@
+const racingBikes= [
+    {
+        name:'Giant Propel Advanced SL',
+        weight:6.8,
+    },
+    {
+        name:'Scott Addict RC Ultimate',
+        weight:6.7
+    },
+    {
+        name:'Specialized S-Works Aethos',
+        weight:6.3,
+    },
+    {
+        name:'Wilier Zero SLR',
+        weight:7.1,
+    },
+
+];
+console.log(racingBikes)
