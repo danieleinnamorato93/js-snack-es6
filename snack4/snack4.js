a9c74e0a6b5d8cd0b554464fb031cdf46bc7f06f
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-30 16:48:23 +0200

    destrutturazione

--- snack4/snack4.js
+++ snack4/snack4.js
@@ -1,4 +1,4 @@
-const teamsFootball = [ 
+const footbalTeams = [ 
     {
         name:'Lipsia',
         points:0,
@@ -25,3 +25,20 @@ const teamsFootball = [
     },
     
 ]
+//creo una funzione per generare numeri randomici
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max-min +1)) +min;
+}
+
+//genero numeri casuali con for each  e la funzione getRandomNumber
+footbalTeams.forEach(team=>{
+    team.points = getRandomNumber(0,100);
+    team.fouls = getRandomNumber(0,50);
+});
+console.log(footbalTeams)
+
+//creo un nuovo array che contiene solo il nome e i falli subiti
+
+const teamsFouls = footbalTeams.map(({name, fouls})  => ({name, fouls}));
+console.log(teamsFouls)
