7df64f8ee65e93a6f44ca0b4c3e7e2059baed7f0
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-09-02 12:36:58 +0200

    fixed

--- snack7/script.js
+++ snack7/script.js
@@ -1,15 +1,15 @@
 const people = [
     { nome: 'Marco', cognome: 'Resini', età: 19 },
     { nome: 'Giulia', cognome: 'Rossi', età: 25 },
-    { nome: 'Luca', cognome: 'Bianchi', età: 30 },
+    { nome: 'Luca', cognome: 'Bianchi', età: 13 },
     { nome: 'Federica', cognome: 'Esposito', età: 22 },
     { nome: 'Andrea', cognome: 'Russo', età: 27 },
-    { nome: 'Chiara', cognome: 'Greco', età: 21 },
+    { nome: 'Chiara', cognome: 'Greco', età: 17 },
     { nome: 'Matteo', cognome: 'Ferrari', età: 34 },
-    { nome: 'Elena', cognome: 'Fontana', età: 28 },
+    { nome: 'Elena', cognome: 'Fontana', età: 9 },
     { nome: 'Alessandro', cognome: 'Marini', età: 23 },
     { nome: 'Sara', cognome: 'Conti', età: 26 },
-    { nome: 'Davide', cognome: 'Moretti', età: 32 }
+    { nome: 'Davide', cognome: 'Moretti', età: 15 }
 ];
 
 const newArray = people.map(person => {
