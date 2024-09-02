e0a793fc20e5a46067662fc037313c288a602a2f
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-09-02 12:22:51 +0200

    array generated

---
+++ snack7/script.js
@@ -0,0 +1,14 @@
+const people = [
+    { nome: 'Marco', cognome: 'Resini', età: 19 },
+    { nome: 'Giulia', cognome: 'Rossi', età: 25 },
+    { nome: 'Luca', cognome: 'Bianchi', età: 30 },
+    { nome: 'Federica', cognome: 'Esposito', età: 22 },
+    { nome: 'Andrea', cognome: 'Russo', età: 27 },
+    { nome: 'Chiara', cognome: 'Greco', età: 21 },
+    { nome: 'Matteo', cognome: 'Ferrari', età: 34 },
+    { nome: 'Elena', cognome: 'Fontana', età: 28 },
+    { nome: 'Alessandro', cognome: 'Marini', età: 23 },
+    { nome: 'Sara', cognome: 'Conti', età: 26 },
+    { nome: 'Davide', cognome: 'Moretti', età: 32 }
+];
+console.log(people)
